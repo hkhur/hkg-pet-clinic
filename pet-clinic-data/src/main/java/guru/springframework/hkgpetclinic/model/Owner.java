c735ea2fb48b4338765c8e7ee123255203de1707
hkhur <khurranahimanshu3@gmail.com>
2021-05-27 03:09:55 +0530

    Project Restructuring

---
+++ pet-clinic-data/src/main/java/guru/springframework/hkgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package guru.springframework.hkgpetclinic.model;
+
+public class Owner extends Person{
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/hkgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package guru.springframework.hkgpetclinic.model;
+
+public class Vet extends Person{
+}
