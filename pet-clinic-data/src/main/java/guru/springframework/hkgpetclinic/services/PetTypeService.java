113222913bd5437ccd8264bd3a5712832ccfb343
hkhur <khurranahimanshu3@gmail.com>
2021-06-06 04:00:33 +0530

    PetTypeService added

---
+++ pet-clinic-data/src/main/java/guru/springframework/hkgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package guru.springframework.hkgpetclinic.services;
+
+import guru.springframework.hkgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/hkgpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,37 @@
+package guru.springframework.hkgpetclinic.services.map;
+
+import guru.springframework.hkgpetclinic.model.PetType;
+import guru.springframework.hkgpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return super.save(petType);
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+
+    }
+
+    @Override
+    public void delete(PetType petType) {
+     super.delete(petType);
+    }
+}
